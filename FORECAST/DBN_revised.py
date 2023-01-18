a56faf1e2e2600f9de9a06c69f0c406b77daf4ca
wzy222012 <3190104110@zju.edu.cn>
2023-01-18 18:18:20 +0800

    commit_1.18 temp commit: DBN much revised

--- FORECAST/DBN_revised.py
+++ FORECAST/DBN_revised.py
@@ -1,6 +1,6 @@
 import numpy as np
 import datetime as dt
-import joblib
+import tensorflow
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -24,7 +24,7 @@ class DBN:
             x_test,
             y_test,
             hidden_layer,
-            learning_rate_rbm=0.0001,
+            learning_rate_rbm=0.001,
             batch_size_rbm=100,
             n_epochs_rbm=30,
             verbose_rbm=1,
@@ -151,7 +151,7 @@ def forecast_main():
     len_Test = -(dt.datetime.strptime(test_start, '%Y-%m-%d') - dt.datetime.strptime(test_end, '%Y-%m-%d')).days
 
     # 导入数据
-    data1 = pd.read_csv('./data1/EnergyUse.csv')
+    data1 = pd.read_excel('./data/test1.xlsx')
     # data1 = data1.set_index('Unnamed: 0')
     # data2 = pd.read_excel('./data/test2.xlsx')
     # # data2 = data2.set_index('Unnamed: 0')
@@ -162,7 +162,7 @@ def forecast_main():
     # data4 = data4.set_index('Unnamed: 0')
     # data_Y = pd.read_excel('D:/研究生资料/供电服务/供电服务2021/数据/工单全业务每日汇总统计3/标签_3340140.xlsx')
 
-    data_la1 = data1.iloc[:, 2]
+    data_la1 = data1.iloc[:, 6]
     # data_la2 = data2.iloc[:, 6]
     # data_la3 = data3.iloc[:, 6]
     # data_la4 = data4.iloc[:, 6]
@@ -171,7 +171,6 @@ def forecast_main():
     max_value1 = np.max(data_la1)
     min_value1 = np.min(data_la1)
     data_la1 = (data_la1 - min_value1) / (max_value1 - min_value1)
-    data_la_train = data_la1
     # max_value2 = np.max(data_la2)
     # min_value2 = np.min(data_la2)
     # data_la2 = (data_la2 - min_value2) / (max_value2 - min_value2)
@@ -184,64 +183,141 @@ def forecast_main():
 
     data_Y = data1.iloc[:, 0]
 
-    # 各种数据长度计算
     total_size = np.size(data_Y)
-    exchange_time = 3                                   # 交叉验证份数
-    exchange_size = int(total_size / exchange_time)     # 交叉验证测试集长度
-    train_size = total_size - exchange_size             # 交叉验证训练集长度
-    rolling_size = 2                                    # 滚动长度
-    print(train_size)
+    print(total_size)
+    train_size = int(total_size * 0.7)
+    rolling_size = 12
 
+    # 导入训练集X_train 和 测试集X_test
+    x_train = np.zeros([rolling_size, train_size - rolling_size])
+    y_train = np.zeros([1, train_size - rolling_size])
+    x_test = np.zeros([rolling_size, total_size - train_size - rolling_size])
+    y_test = np.zeros([1, total_size - train_size - rolling_size])
+    for i in range(train_size - rolling_size):
+        for j in range(rolling_size):
+            x_train[j, i] = data_la1[i+j]
+        y_train[0, i] = data_la1[i+rolling_size]
+    for i in range(total_size - train_size - rolling_size):
+        for j in range(rolling_size):
+            x_test[j, i] = data_la1[train_size + i + j]
+        y_test[0, i] = data_la1[train_size + i + rolling_size]
+    x_train = x_train.T
+    y_train = y_train.T
+    x_test = x_test.T
+    y_test = y_test.T
+    print(x_train, y_train)
+    # mae_all = []
+    # mse_all = []
 
-    for k in range(exchange_time):
-        # 新建训练集X_train 和 测试集X_test
-        x_train = np.zeros([rolling_size, train_size - rolling_size])
-        y_train = np.zeros([1, train_size - rolling_size])
-        x_test = np.zeros([rolling_size, exchange_size - rolling_size])
-        y_test = np.zeros([1, exchange_size - rolling_size])
+    # 对模型进行训练和保存
+    #     trained_model, scaler = multioutput(df.values, 0.9, 192, 1, 200, 300, 50)
+    model_train(x_train, y_train, x_test, y_test)
 
-        # 滚动交叉验证测试集位置
-        data_la_train = data_la1.drop(range(exchange_size * k, exchange_size * (k + 1)))
-        data_la_train = data_la_train.reset_index(drop=True)
-        data_la_test = data_la1[exchange_size * k: exchange_size * (k + 1)]
+    # 调用模型计算
+    model = load_model(r'./data/trained_DBN.h5')
+    pre = model.predict(data_la1)
+    # pre = pre * (max_value1 - min_value1) + min_value1
+    # y_test = y_test * (max_value1 - min_value1) + min_value1
+    pre_max = np.max(pre)
+    # date_start, date_end = '2020-01-11', '2021-06-24'
+    # Time_list = nday_list(date_start, date_end).tolist()
+    # len_T = -(dt.datetime.strptime(date_start, '%Y-%m-%d') - dt.datetime.strptime(date_end, '%Y-%m-%d')).days
+    # test_start, test_end = str(dt.datetime.strptime(date_start, '%Y-%m-%d') + dt.timedelta(days=371))[:-9], '2021-06-24'
+    # Test_list = nday_list(test_start, test_end).tolist()
+    # len_Test = -(dt.datetime.strptime(test_start, '%Y-%m-%d') - dt.datetime.strptime(test_end, '%Y-%m-%d')).days
 
-        # 导入训练集X_train 和 测试集X_test
-        for i in range(train_size - rolling_size):
-            for j in range(rolling_size):
-                x_train[j, i] = data_la_train.iloc[i + j]
-            y_train[0, i] = data_la_train.iloc[i + rolling_size]
-        for i in range(exchange_size - rolling_size):
-            for j in range(rolling_size):
-                x_test[j, i] = data_la_test.iloc[i + j]
-            y_test[0, i] = data_la_test.iloc[i + rolling_size]
-
-        # 修下格式
-        x_train = x_train.T
-        y_train = y_train.T
-        x_test = x_test.T
-        y_test = y_test.T
-
-        print(x_train, y_train, x_test, y_test)
-
-        # 对模型进行训练和保存
-        #     trained_model, scaler = multioutput(df.values, 0.9, 192, 1, 200, 300, 50)
-        model_train(x_train, y_train, x_test, y_test)
-
-        # 调用模型计算
-        model = load_model(r'./data/trained_DBN.h5')
-        pre = model.predict(x_test)
-        if k == 0:
-            pre_size = pre.size
-            predic = np.zeros([pre_size, 1])
-        predic += pre
-        pre_max = np.max(pre)
-    predic /= exchange_time
-    print(predic, pre, y_test)
-    # for i in range(total_size - train_size):
-    #     if pre[i] > pre_max / 2:
-    #         pre[i] = 1
-    #     else:
-    #         pre[i] = 0
+    # # 导入数据
+    # data1 = pd.read_excel('./data/test1.xlsx')
+    # # data1 = data1.set_index('Unnamed: 0')
+    # # data2 = pd.read_excel('./data/test2.xlsx')
+    # # # data2 = data2.set_index('Unnamed: 0')
+    # # data3 = pd.read_excel('./data/test3.xlsx')
+    # # # data3 = data3.set_index('Unnamed: 0')
+    # # data4 = pd.read_excel('./data/test4.xlsx')
+    #
+    # # data4 = data4.set_index('Unnamed: 0')
+    # # data_Y = pd.read_excel('D:/研究生资料/供电服务/供电服务2021/数据/工单全业务每日汇总统计3/标签_3340140.xlsx')
+    #
+    # data_la1 = data1.iloc[:, 6]
+    # # data_la2 = data2.iloc[:, 6]
+    # # data_la3 = data3.iloc[:, 6]
+    # # data_la4 = data4.iloc[:, 6]
+    #
+    # # 归一化
+    # max_value1 = np.max(data_la1)
+    # min_value1 = np.min(data_la1)
+    # data_la1 = (data_la1 - min_value1) / (max_value1 - min_value1)
+    # data_la_train = data_la1
+    # # max_value2 = np.max(data_la2)
+    # # min_value2 = np.min(data_la2)
+    # # data_la2 = (data_la2 - min_value2) / (max_value2 - min_value2)
+    # # max_value3 = np.max(data_la3)
+    # # min_value3 = np.min(data_la3)
+    # # data_la3 = (data_la3 - min_value3) / (max_value3 - min_value3)
+    # # max_value4 = np.max(data_la4)
+    # # min_value4 = np.min(data_la4)
+    # # data_la4 = (data_la4 - min_value4) / (max_value4 - min_value4)
+    #
+    # data_Y = data1.iloc[:, 0]
+    #
+    # # 各种数据长度计算
+    # total_size = np.size(data_Y)
+    # exchange_time = 3                                   # 交叉验证份数
+    # exchange_size = int(total_size / exchange_time)     # 交叉验证测试集长度
+    # train_size = total_size - exchange_size             # 交叉验证训练集长度
+    # rolling_size = 5                                    # 滚动长度
+    # print(train_size)
+    #
+    #
+    # for k in range(exchange_time):
+    #     # 新建训练集X_train 和 测试集X_test
+    #     x_train = np.zeros([rolling_size, train_size - rolling_size])
+    #     y_train = np.zeros([1, train_size - rolling_size])
+    #     x_test = np.zeros([rolling_size, exchange_size - rolling_size])
+    #     y_test = np.zeros([1, exchange_size - rolling_size])
+    #
+    #     # 滚动交叉验证测试集位置
+    #     data_la_train = data_la1.drop(range(exchange_size * k, exchange_size * (k + 1)))
+    #     data_la_train = data_la_train.reset_index(drop=True)
+    #     data_la_test = data_la1[exchange_size * k: exchange_size * (k + 1)]
+    #
+    #     # 导入训练集X_train 和 测试集X_test
+    #     for i in range(train_size - rolling_size):
+    #         for j in range(rolling_size):
+    #             x_train[j, i] = data_la_train.iloc[i + j]
+    #         y_train[0, i] = data_la_train.iloc[i + rolling_size]
+    #     for i in range(exchange_size - rolling_size):
+    #         for j in range(rolling_size):
+    #             x_test[j, i] = data_la_test.iloc[i + j]
+    #         y_test[0, i] = data_la_test.iloc[i + rolling_size]
+    #
+    #     # 修下格式
+    #     x_train = x_train.T
+    #     y_train = y_train.T
+    #     x_test = x_test.T
+    #     y_test = y_test.T
+    #
+    #     print(x_train, y_train, x_test, y_test)
+    #
+    #     # 对模型进行训练和保存
+    #     #     trained_model, scaler = multioutput(df.values, 0.9, 192, 1, 200, 300, 50)
+    #     model_train(x_train, y_train, x_test, y_test)
+    #
+    #     # 调用模型计算
+    #     model = load_model(r'./data/trained_DBN.h5')
+    #     pre = model.predict(x_test)
+    #     if k == 0:
+    #         pre_size = pre.size
+    #         predic = np.zeros([pre_size, 1])
+    #     predic += pre
+    #     pre_max = np.max(pre)
+    # predic /= exchange_time
+    # print(predic, pre, y_test)
+    # # for i in range(total_size - train_size):
+    # #     if pre[i] > pre_max / 2:
+    # #         pre[i] = 1
+    # #     else:
+    # #         pre[i] = 0
 
     # 作图
     plt.rcParams['font.sans-serif'] = ['KaiTi']
@@ -250,8 +326,8 @@ def forecast_main():
     # x_tick = Time_list[train_size:]
     # plt.plot(x_tick, pre, 'r', label='prediction')
     # plt.plot(x_tick, y_test, 'b', label='real')
-    plt.plot(data_Y[train_size: total_size - rolling_size], pre, 'b', label='predict')
-    plt.plot(data_Y[train_size: total_size - rolling_size], predic, 'r', label='prediction')
+    plt.plot(data_Y[train_size: total_size - rolling_size], predic, 'b', label='predict')
+    # plt.plot(data_Y[train_size: total_size - rolling_size], predic, 'r', label='prediction')
     plt.plot(data_Y[train_size: total_size - rolling_size], y_test, 'g', label='real')
     # plt.plot(data_Y[train_size: total_size + 1], y_test, 'b', label='real')
     plt.tick_params(axis='y', labelcolor='k')
